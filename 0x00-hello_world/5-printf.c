831b47e8748a1b1b891fa440d080e52ee4a785fa
Amanugk <amanugk@gmail.com>
2023-02-17 07:38:44 -0800

    5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - This program prints a line using puts
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+        printf("\"Programming is like building a multilingual puzzle\n");
+        return (0);
+}
