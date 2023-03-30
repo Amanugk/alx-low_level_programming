ff3903b9bd71111ab73538a6d7a28ae19de76bba
Amanugk <amanugk@gmail.com>
2023-03-30 08:24:13 -0700

    swap dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,13 +1,14 @@
-#ifndef BEHAILU
-#define BEHAILU
+#ifndef DOG_H
+#define DOG_H
 
 /**
-  * struct dog - Dog attributes
-  * @name: The name of the dog.
-  * @age: The age of the dog.
-  * @owner: The owner of the dog.
-  * Description: The attributes of a dog.
-  */
+ * struct dog - a new type of data
+ * representing a dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner's nam/
+ */
+
 struct dog
 {
 	char *name;
@@ -15,8 +16,10 @@ struct dog
 	char *owner;
 };
 
-/**
- * dog_t - Typedef for dog structure
- */
 typedef struct dog dog_t;
-#endif
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif /*DOG_H*/
